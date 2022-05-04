90b71ad690eaff3cbddfead97ad4870dd4b1e56c
Joao Victor M <joaovmvale@gmail.com>
2022-05-04 17:53:32 -0300

    Initial commit

---
+++ iFinance/iFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFinanceApp.swift
+//  iFinance
+//
+//  Created by ALUNO on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
